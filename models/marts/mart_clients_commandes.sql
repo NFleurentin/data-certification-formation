595e963d91784326163f945cd961e49ea72b2432
NFleurentin <fleurentin.nicolas@gmail.com>
2026-09-13 17:23:10 +0200

    Add SQL script for mart_clients_commandes to aggregate client orders

---
+++ models/marts/mart_clients_commandes.sql
@@ -0,0 +1,26 @@
+WITH
+    clients AS (
+        SELECT
+            *
+        FROM
+            {{ref ('stg_customers')}}
+    ),
+    commandes AS (
+        SELECT
+            *
+        FROM
+            {{ref ('stg_orders')}}
+    )
+SELECT
+    c.client_id,
+    c.prenom,
+    c.nom,
+    COUNT(co.commande_id) AS nombre_commandes,
+    SUM(co.montant) AS montant_total
+FROM
+    clients c
+    LEFT JOIN commandes co ON c.client_id = co.client_id
+GROUP BY
+    c.client_id,
+    c.prenom,
+    c.nom;
