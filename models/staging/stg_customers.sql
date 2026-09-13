6dc93d8490d63fb623268c4e2ca4a2fd78de508f
NFleurentin <fleurentin.nicolas@gmail.com>
2026-09-13 17:18:17 +0200

    Add staging SQL script for customers data transformation

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,21 @@
+WITH
+    source AS (
+        SELECT
+            *
+        FROM
+            {{source ('raw', 'customers')}}
+    ),
+    renamed AS (
+        SELECT
+            id AS client_id,
+            first_name AS prenom,
+            last_name AS nom,
+            lower(email) AS email,
+            created_at AS date_creation
+        FROM
+            source
+    )
+SELECT
+    *
+FROM
+    renamed;
