94eadbe004fd79172cf90096aa61579035b9bd67
NFleurentin <fleurentin.nicolas@gmail.com>
2026-09-13 17:18:22 +0200

    Add SQL script for staging orders data transformation

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+WITH
+    source AS (
+        SELECT
+            *
+        FROM
+            {{source ('raw', 'orders')}}
+    ),
+    renamed AS (
+        SELECT
+            order_id AS commande_id,
+            customer_id AS client_id,
+            amount AS montant,
+            status AS statut,
+            order_date AS date_commande
+        FROM
+            source
+    )
+SELECT
+    *
+FROM
+    renamed;
