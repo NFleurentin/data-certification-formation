8b768794087fae25c65f96b9f29ace710b66b07b
NFleurentin <fleurentin.nicolas@gmail.com>
2026-09-13 17:03:47 +0200

    First commit

---
+++ models/mon_premier_modele.sql
@@ -0,0 +1,14 @@
+SELECT
+    1 AS id,
+    'Marc' AS nom,
+    'Paris' AS ville
+UNION ALL
+SELECT
+    2 AS id,
+    'Sophie' AS nom,
+    'Lyon' AS ville
+UNION ALL
+SELECT
+    3 AS id,
+    'Karim' AS nom,
+    'Marseille' AS ville
